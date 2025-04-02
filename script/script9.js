d06d5395089f5612fa0a31e2ea0e7147a3f51237
Technorithm <technorithmgwcet123@gmail.com>
2025-04-02 12:38:33 +0530

    `Added new event page and explore page and gallery updates !!`

--- script/script9.js
+++ script/script9.js
@@ -22,20 +22,20 @@ document.addEventListener("click", function (event) {
 });
 
 // Alert for Upcoming Events
-document
-  .getElementById("upcoming-event")
-  .addEventListener("click", function (event) {
-    event.preventDefault();
-    Swal.fire({
-      title: "Coming Soon!",
-      html: "<p style='font-size: 1.5rem;'>🚀 Something BIG is on the way! Stay tuned for mind-blowing events! 🎉</p>",
-      icon: "info",
-      confirmButtonText: "OK",
-      background: "#fff",
-      color: "#333",
-      confirmButtonColor: "#007bff",
-      showClass: { popup: "animate__animated animate__bounceIn" },
-      hideClass: { popup: "animate__animated animate__bounceOut" },
-    });
-  });
+// document
+//   .getElementById("upcoming-event")
+//   .addEventListener("click", function (event) {
+//     event.preventDefault();
+//     Swal.fire({
+//       title: "Coming Soon!",
+//       html: "<p style='font-size: 1.5rem;'>🚀 Something BIG is on the way! Stay tuned for mind-blowing events! 🎉</p>",
+//       icon: "info",
+//       confirmButtonText: "OK",
+//       background: "#fff",
+//       color: "#333",
+//       confirmButtonColor: "#007bff",
+//       showClass: { popup: "animate__animated animate__bounceIn" },
+//       hideClass: { popup: "animate__animated animate__bounceOut" },
+//     });
+//   });
 
